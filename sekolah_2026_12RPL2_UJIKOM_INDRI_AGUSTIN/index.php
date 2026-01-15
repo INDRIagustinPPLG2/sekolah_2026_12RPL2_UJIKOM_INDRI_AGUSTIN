6969d0c3e05cdf0a111c5bbf55b0815afb8af903
INDRIagustinPPLG2 <152269117+INDRIagustinPPLG2@users.noreply.github.com>
2026-01-15 16:13:18 +0700

    index.php

--- sekolah_2026_12RPL2_UJIKOM_INDRI_AGUSTIN/index.php
+++ sekolah_2026_12RPL2_UJIKOM_INDRI_AGUSTIN/index.php
@@ -1,19 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <style>
+      body{
+            background-color: #B1C9EF;
+      }
+      div{
+       margin: 100px;
+ 
+      }
+      h1 {
+            text-align: center;
+      }
+      button {
+         width: 125pxpx; /* Lebar tetap 200 piksel */
+  height: 40px;
+      }
+      .pengaduan {
+        background-color: #fbe290;
+        color:#bf7e46;
+      }
+      .halaman{
+        background-color: #fbedd6;
+        color : #bf7e46;
+      }
+    </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <h1>SELAMAT DATANG DI WEBSITE pengaduan MUTU</h1>
-   <a href="form_pengaduan.php">
-  <button>buat pengaduan</button>
-</a>
- <a href="cari_pengaduan.php">
-  <button>cari pengaduan</button>
-</a>
-
+  <div>
+    <h1>SELAMAT DATANG DI WEBSITE PENGADUAN MUTU</h1>
+   <center><a href="form_pengaduan.php"><button class="pengaduan">buat pengaduan</button></a>
+ <a href="cari_pengaduan.php"><button class="halaman">cari pengaduan</button></a></center>
+</div>
     
 </body>
 </html>
