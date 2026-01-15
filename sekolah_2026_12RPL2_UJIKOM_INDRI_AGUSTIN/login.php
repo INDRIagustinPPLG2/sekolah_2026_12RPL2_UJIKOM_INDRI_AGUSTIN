93dc381df768fa765aefeeadf51e3f0a9a2ee337
INDRIagustinPPLG2 <152269117+INDRIagustinPPLG2@users.noreply.github.com>
2026-01-15 16:12:11 +0700

    login.php

---
+++ sekolah_2026_12RPL2_UJIKOM_INDRI_AGUSTIN/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <h1>login</h1>
+    <form action="">
+        <div>
+        <label for="">username</label> <br/>
+        <input type="text" />
+        </div>
+         <div>
+        <label for="">password</label> <br/>
+        <input type="text" />
+        </div>
+        <div>
+        <label for="">role</label> <br/>
+        <select>
+            <option >siswa</option>
+            <option>admin</option>
+        </select>
+        </div>
+         <div>
+        <label for="">NIS</label> <br/>
+        <input type="text" />
+        </div>
+         <div>
+        <label for="">kelas</label> <br/>
+        <input type="text" />
+        </div>
+    </form>
+    </center>
+</body>
+</html>
