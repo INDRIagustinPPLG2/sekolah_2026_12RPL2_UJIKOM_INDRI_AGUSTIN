216ce8da8bec064ae3501477fc8b2accc11e4ae1
INDRIagustinPPLG2 <152269117+INDRIagustinPPLG2@users.noreply.github.com>
2026-01-15 13:37:34 +0700

    Add files via upload

---
+++ sekolah_2026_12RPL2_UJIKOM_INDRI_AGUSTIN/form_pengaduan.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>halaman pengaduan</h1>
+   <form action="">
+        <div>
+        <label for="">NIS</Llabel> <br/>
+        <input type="text" />
+        </div>
+        <div>
+        <label for="">Kategori</Llabel> <br/>
+        <input type="text" />
+        </div>
+        <div>
+        <label for="">lokasi</Llabel> <br/>
+        <input type="text" />
+        </div>
+        <div>
+        <label for="">keterangan</Llabel> <br/>
+        <textarea></textarea>
+        </div>
+    </form>
+</body>
+</html>
---
+++ sekolah_2026_12RPL2_UJIKOM_INDRI_AGUSTIN/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>SELAMAT DATANG DI WEBSITE pengaduan MUTU</h1>
+   <a href="form_pengaduan.php">
+  <button>buat pengaduan</button>
+</a>
+ <a href="cari_pengaduan.php">
+  <button>cari pengaduan</button>
+</a>
+
+    
+</body>
+</html>
