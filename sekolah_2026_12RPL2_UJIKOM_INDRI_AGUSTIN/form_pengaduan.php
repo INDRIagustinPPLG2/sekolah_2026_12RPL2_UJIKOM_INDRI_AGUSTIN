265fb00dbb023d905ccba3eb30a6e1c62ea218e6
INDRIagustinPPLG2 <152269117+INDRIagustinPPLG2@users.noreply.github.com>
2026-01-15 16:15:07 +0700

    form_pengaduan.php

--- sekolah_2026_12RPL2_UJIKOM_INDRI_AGUSTIN/form_pengaduan.php
+++ sekolah_2026_12RPL2_UJIKOM_INDRI_AGUSTIN/form_pengaduan.php
@@ -6,24 +6,35 @@
     <title>Document</title>
 </head>
 <body>
+    <center>
     <h1>halaman pengaduan</h1>
    <form action="">
         <div>
-        <label for="">NIS</Llabel> <br/>
+        <label for="">NIS</label> <br/>
         <input type="text" />
         </div>
         <div>
-        <label for="">Kategori</Llabel> <br/>
+        <label for="">Kategori</label> <br/>
+        <select>
+            <option >Fasilitas</option>
+            <option>Lingkunagn</option>
+        </select>
+        </div>
+        <div>
+        <label for="">lokasi</label> <br/>
         <input type="text" />
         </div>
         <div>
-        <label for="">lokasi</Llabel> <br/>
-        <input type="text" />
-        </div>
-        <div>
-        <label for="">keterangan</Llabel> <br/>
+        <label for="">keterangan</label> <br/>
         <textarea></textarea>
         </div>
+        <div>
+        <button>kirim</button>
+        </div>
+         <div>
+        <a href="detail_pengaduan.php"><button>detail pengaduan</button></a>
+        </div>
     </form>
+    </center>
 </body>
 </html>
