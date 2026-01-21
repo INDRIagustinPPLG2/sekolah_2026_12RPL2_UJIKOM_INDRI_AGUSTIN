8322f185f055c0b9e60292785f0b12ec77140eb2
INDRIagustinPPLG2 <152269117+INDRIagustinPPLG2@users.noreply.github.com>
2026-01-21 16:54:31 +0700

    halaman-utama.php

---
+++ sekolah_2026_12RPL2_UJIKOM_INDRI_AGUSTIN/admin/halaman-utama.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+      body{
+            background-color: #B1C9EF;
+      }
+      div{
+       margin: 100px;
+ 
+      }
+      h1 {
+            text-align: center;
+      }
+      button {
+         width: 125pxpx; /* Lebar tetap 200 piksel */
+  height: 40px;
+      }
+      .pengaduan {
+        background-color: #fbe290;
+        color:#bf7e46;
+      }
+      .halaman{
+        background-color: #fbedd6;
+        color : #bf7e46;
+      }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <div>
+    <h1>SELAMAT DATANG ADMIN DI WEBSITE PENGADUAN MUTU</h1>
+   <center><a href="detail_pengaduanadmin.php"><button class="pengaduan">detail pengaduan</button></a>
+ <a href="data_pengaduan.php"><button class="halaman">data pengaduan</button></a></center>
+</div>
+    
+</body>
+</html>
