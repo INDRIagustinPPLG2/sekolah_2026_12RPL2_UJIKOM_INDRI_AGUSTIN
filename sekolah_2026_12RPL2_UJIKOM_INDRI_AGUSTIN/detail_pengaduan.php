007a0328a482fc66c87b54115fd600f3e20f8e16
INDRIagustinPPLG2 <152269117+INDRIagustinPPLG2@users.noreply.github.com>
2026-02-11 17:12:04 +0700

    detail_pengaduan.php

--- sekolah_2026_12RPL2_UJIKOM_INDRI_AGUSTIN/detail_pengaduan.php
+++ sekolah_2026_12RPL2_UJIKOM_INDRI_AGUSTIN/detail_pengaduan.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    haloo
-</body>
-</html>
+<?php
+$koneksi = mysqli_connect("localhost","root","", "ujikom_12rpl2_indriagustin");
+
+$sql ="select * from `input_aspirasi`";
+
+$query = mysqli_query($koneksi, $sql);
+
+// perulangan
+while($data = mysqli_fetch_assoc($query)){ ?>
+<p>Lokasi <?php echo $data['lokasi']; ?> </p>
+<p>Keterangan <?php echo $data['keterangan']; ?> </p>
+<p>status <?php echo $data['ststus']; ?> 
+<hr/>
+<?php } ?>
