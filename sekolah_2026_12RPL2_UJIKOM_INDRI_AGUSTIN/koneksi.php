333584adba3130624453f0f3119e720649707e0d
INDRIagustinPPLG2 <152269117+INDRIagustinPPLG2@users.noreply.github.com>
2026-02-11 16:32:25 +0700

    koneksi.php

--- sekolah_2026_12RPL2_UJIKOM_INDRI_AGUSTIN/koneksi.php
+++ sekolah_2026_12RPL2_UJIKOM_INDRI_AGUSTIN/koneksi.php
@@ -1,24 +1,7 @@
 <?php
-if isset($_POST['username']) {
-
-
-     $nama       =$_POST['username'];
-     $code  =password_hash($_POST['password'],PASSWORD_DEFAULT); 
-     $peran  =$_POST['role'];
-     $nomor   =$_POST['nis'];
-     $ruangan =$_POST['kelas'];
-
-
-$koneksi = mysqli_connect("localhost","root","","sekolah_2026_12rpl2_ujikom_indri_agustin");
-
-mysqli_query($koneksi,"INSERT INTO user (id, username, password, role, nis, kelas) VALUES (NULL, '$nama', '$code', '$peran', '$nomor', '$ruangan')");
+$koneksi = mysqli_connect("localhost","root","","ujikom_12rpl2_indriagustin");
+if(!$koneksi){
+    echo "koneksi database gagal";
 }
-   
-
-
-// $koneksi = mysqli_connect("localhost","root","","ujikom_12rpl2_indriagustin");
-
-// mysqli_query($koneksi, "INSERT INTO `input_aspirasi` (`id_pelaporan`, `nis`, `id_kategori`, `lokasi`, `keterangan`, `status`, `feedback`) 
-// VALUES (NULL, '006', '1', 'kalau', 'nanti', 'menunggu', NULL)");
 
 ?>
